febebe4e31c9056fcc415baa7cdb77342f5cdac4
Alissa <Alissa@Admins-MacBook-Air-2.local>
2019-10-01 17:18:24 -0400

    updates

--- src/CatName.js
+++ src/CatName.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import {CatNameImage, CatNameContainer} from './styles.js';
-
 import catinator from './images/catinator.png';
 import furnando from './images/furnando.png';
 import shakespurr from './images/shakespurr.png';
 import skywhisker from './images/skywhisker.png';
-// import furnando from './images/furnando.png';
-// import all the images
 
 const catNamesArray = [catinator, furnando, shakespurr, skywhisker];
 
@@ -19,6 +16,4 @@ function CatName() {
 	)
 }
 
-
-// names[Math.floor(Math.random() * names.length)];
 export default CatName;
